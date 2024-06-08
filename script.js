d219c79bf283ce0dbb03bbd01c78ad2dda5baee1
phornnato <natophorn@gmail.com>
2024-06-08 09:26:21 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function(){
+    window.func2 = function() {
+        var change = document.getElementById("demo");
+        if (change) {
+            change.innerHTML = "<p>i love you</p><img id='pic' src='eli.gif'/>";
+        }
+    };
+    window.func3 = function() {
+        var change = document.getElementById("demo");
+        if (change) {
+            change.innerHTML = "<p>i hate you</p><img id='pic' src='hello2.gif'/>";
+        }
+    };
+    window.func4 = function() {
+        var change = document.getElementById("demo");
+        if (change) {
+            change.innerHTML = "<p>will you married me, hik hik </p><img id='pic' src='https://media.tenor.com/y_nhpANxpPYAAAAi/kiss-cute.gif'/>";
+        }
+    };
+});
